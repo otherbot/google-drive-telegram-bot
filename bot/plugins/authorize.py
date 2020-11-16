e4850fa75a9c6f402e7393008001a86f29489ee9
otherbot <72908892+otherbot@users.noreply.github.com>
2020-11-16 10:20:57 +0700

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -16,8 +16,8 @@ from bot.helpers.utils import CustomFilters
 OAUTH_SCOPE = "https://www.googleapis.com/auth/drive"
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-G_DRIVE_CLIENT_ID = "202264815644.apps.googleusercontent.com"
-G_DRIVE_CLIENT_SECRET = "X4Z3ca8xfWDb1Voo-F9a7ZxJ"
+G_DRIVE_CLIENT_ID = "905663246750-ffsstimqnml82tnusg7r4lptjqlfbpf1.apps.googleusercontent.com"
+G_DRIVE_CLIENT_SECRET = "TOLd7Bd19XNDWF6ExVhV1TwH"
 flow = None
 
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.Authorize))
